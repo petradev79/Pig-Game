7dddde906304fd267698ac0222429329dcf3d709
Ivan Petrovic <61566864+petradev79@users.noreply.github.com>
2020-11-24 15:05:56 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.description-button').addEventListener('click', function(e) {
+    document.querySelector('.description-wrapper').classList.toggle('hidden');
+    document.querySelector('.description-menu').classList.toggle('up');
+});
